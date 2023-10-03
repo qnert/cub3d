83c730b6062b78cf02a27b8854d3f2d7d4f84c61
Simon Kunert <skunert@1-A-2.42heilbronn.de>
2023-10-03 21:41:46 +0200

    error msg added and norminette fix

--- srcs/utils/parsing_utils.c
+++ srcs/utils/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:27:36 by skunert           #+#    #+#             */
-/*   Updated: 2023/10/03 21:23:41 by skunert          ###   ########.fr       */
+/*   Updated: 2023/10/03 21:36:20 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,10 @@ bool	is_valid_border(char c)
 		if (c != '1')
 			return (false);
 	return (true);
+}
+
+void	ft_error_msg(char *str)
+{
+	write(2, "Error\n", 6);
+	write(2, str, ft_strlen(str));
 }
