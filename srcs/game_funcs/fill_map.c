ad0bd3fecf5d658315fd5ff05e1f4d3777794f45
Simon Kunert <skunert@1-A-2.42heilbronn.de>
2023-10-06 15:32:43 +0200

    map parsing and checking map and player position for better visualization of the raycasting stuff finished

---
+++ srcs/game_funcs/fill_map.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fill_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 14:38:02 by skunert           #+#    #+#             */
+/*   Updated: 2023/10/06 15:32:11 by skunert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+void	ft_fill_wall_and_space(t_game *game, char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == '1')
+				mlx_image_to_window(game->mlx, game->wall, j * 50, i * 50);
+			else if (map[i][j] == 'E' || map[i][j] == 'W'
+				|| map[i][j] == 'S' || map[i][j] == 'N')
+				mlx_image_to_window(game->mlx, game->player,
+					j * 50 + 20, i * 50 + 20);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_fill_map(t_game *game, char **map)
+{
+	ft_fill_wall_and_space(game, map);
+	mlx_image_to_window(game->mlx, game->player, 0, 0);
+}
--- srcs/game_funcs/game_init.c
+++ srcs/game_funcs/game_init.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 13:22:45 by skunert           #+#    #+#             */
-/*   Updated: 2023/10/06 14:18:56 by skunert          ###   ########.fr       */
+/*   Updated: 2023/10/06 15:27:40 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	get_longest_line(char **matrix)
 	unsigned int	i;
 
 	check = ft_strlen(matrix[0]);
-	i = 1;
+	i = 0;
 	while (matrix[i])
 	{
 		if (check < ft_strlen(matrix[i]))
@@ -28,10 +28,11 @@ int	get_longest_line(char **matrix)
 	return (check);
 }
 
-int	ft_game_begin(t_game *game)
+int	ft_game_begin(t_game *game, char **map)
 {
-	game->mlx = mlx_init(game->width, game->height, "Raycast", false);
-	get_player_img(game);
+	game->mlx = mlx_init(game->width * 50, game->height * 50, "Raycast", true);
+	get_images(game);
+	ft_fill_map(game, map);
 	mlx_loop_hook(game->mlx, ft_move, game);
 	mlx_loop(game->mlx);
 	mlx_terminate(game->mlx);
@@ -40,8 +41,8 @@ int	ft_game_begin(t_game *game)
 
 void	game_init(t_map *map)
 {
-	map->game->width = get_longest_line(map->map) * 50;
-	map->game->height = ft_matrixlen(map->map) * 50;
-	ft_game_begin(map->game);
+	map->game->width = get_longest_line(map->map);
+	map->game->height = ft_matrixlen(map->map);
+	ft_game_begin(map->game, map->map);
 	return ;
 }
